27bdc3e2f597bba3db1b9f889fe64d7d27096e92
Erick Malta <erickmalta100@gmail.com>
2020-06-18 23:59:17 -0300

    Added new components and styles

---
+++ src/components/ServerButton/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+import { Props } from ".";
+
+export const Button = styled.button<Props>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  /* O botão não pode ser espremido, dessa forma */
+`;
---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { Container, Separator } from "./styles";
+import ServerButton from "../ServerButton";
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      <ServerButton isHome />
+      <Separator />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton mentions={3} />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton mentions={5} />
+      <ServerButton hasNotifications />
+      <ServerButton mentions={12} />
+      <ServerButton />
+    </Container>
+  );
+};
+
+export default ServerList;
